18817c57ca0d7a13e8d7998704b9f29ce9ca28f6
Alex <88356153+Alexxmfs@users.noreply.github.com>
2023-08-20 22:08:02 -0300

    finish page qualification

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -55,7 +55,7 @@ const Data = () => {
         </svg>
         </h1>
         <h3 className="home__subtitle">Frontend Developer</h3>
-        <p className="home__description">I'm creative designer based in New York, and I'm very passionate and dedicated to my work.</p>
+        <p className="home__description">Tenho 18 anos e estou estudando Sistemas de Informação na ESPM. Minha jornada acadêmica no bacharelado reflete minha paixão pela tecnologia e meu desejo de aprofundar meu conhecimento nessa área.</p>
 
         <a href="#contact" className="button button--flex">
             Diga Olá
--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -10,18 +10,18 @@ const Qualification = () => {
   return (
     <section className='qualification section'>
         <h2 className='section__title'>Qualification</h2>
-        <span className='section__subtitle'>My personal journey</span>
+        <span className='section__subtitle'>Minha jornada pessoal</span>
 
         <div className="qualification__container container">
             <div className="qualification__tabs">
                 <div className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
                 onClick={() => toggleTab(1)}>
-                    <i className="uil uil-graduation-cap qualification__icon">Education</i>
+                    <i className="uil uil-graduation-cap qualification__icon">Educação</i>
                 </div>
                 
                 <div className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
                 onClick={() => toggleTab(2)}>
-                    <i className="uil uil-briefcase-alt qualification__icon">Experience</i>
+                    <i className="uil uil-briefcase-alt qualification__icon">Trabalho</i>
                 </div>
             </div>
 
